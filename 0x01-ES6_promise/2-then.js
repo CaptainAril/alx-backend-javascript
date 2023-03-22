09be7da6801e4e275673576049bc074de34a2cd0
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-03-22 12:31:47 +0100

    use promise object handlers

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => {
+      const obj = { status: 200, body: 'success' };
+      return obj;
+    })
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
