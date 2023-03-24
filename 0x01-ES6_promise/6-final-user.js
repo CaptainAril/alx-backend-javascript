1fae21e33935db05f8261c53feea6314dab611ba
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-03-24 22:49:57 +0100

    handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,32 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = {
+    status: 'pending',
+  };
+
+  const promise2 = {
+    status: 'pending',
+  };
+
+  try {
+    const signUp = await signUpUser(firstName, lastName);
+    promise1.status = 'fulfilled';
+    promise1.value = signUp;
+  } catch (err) {
+    promise1.status = 'rejected';
+    promise1.value = err.toString();
+  }
+
+  try {
+    const upload = await uploadPhoto(fileName);
+    promise2.status = 'fulfilled';
+    promise2.value = upload;
+  } catch (err) {
+    promise2.status = 'rejected';
+    promise2.value = err.toString();
+  }
+
+  return [promise1, promise2];
+}
