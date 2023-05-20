08e61bafab1e317af7ae62d2495c2af51b6042e6
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-05-20 21:30:37 +0100

    read file asynchronously

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 
-async function countStudents(file) {
+function countStudents(file) {
   return new Promise((resolve, reject) => {
-    fs.readFile(file, 'utf-8', (err, content) => {
+    fs.readFile(file, 'utf-8', (err, data) => {
       if (err) {
-        console.log(err)
         reject(Error('Cannot load the database'));
         return;
       }
       const CS = [];
       const SWE = [];
-      content = content.split('\n');
+      const content = data.split('\n');
       content.shift();
       while (content[content.length - 1] === '') {
         content.pop();
@@ -27,9 +26,10 @@ async function countStudents(file) {
       }
       console.log(`Number of students in CS: ${CS.length}. List: ${CS.join(', ')}`);
       console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`);
-    })
-  
-  })
+
+      resolve();
+    });
+  });
 }
 
 module.exports = countStudents;
