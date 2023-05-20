b9564657d8b312e9e3790ff291861d3aba33b8cf
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-05-20 21:12:25 +0100

    read file asynchronously

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,31 +1,35 @@
-const fs = require('fs/promises');
+const fs = require('fs');
 
 async function countStudents(file) {
-  let content;
-  const CS = [];
-  const SWE = [];
-  try {
-    content = await fs.readFile(file, 'utf-8');
-    content = content.split('\n');
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
-  content.shift();
-  while (content[content.length - 1] === '') {
-    content.pop();
-  }
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf-8', (err, content) => {
+      if (err) {
+        console.log(err)
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      const CS = [];
+      const SWE = [];
+      content = content.split('\n');
+      content.shift();
+      while (content[content.length - 1] === '') {
+        content.pop();
+      }
 
-  console.log(`Number of students: ${content.length}`);
+      console.log(`Number of students: ${content.length}`);
 
-  for (const student of content) {
-    if (student.includes('CS')) {
-      CS.push(student.split(',')[0]);
-    } else if (student.includes('SWE')) {
-      SWE.push(student.split(',')[0]);
-    }
-  }
-  console.log(`Number of students in CS: ${CS.length}. List: ${CS.join(', ')}`);
-  console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`);
+      for (const student of content) {
+        if (student.includes('CS')) {
+          CS.push(student.split(',')[0]);
+        } else if (student.includes('SWE')) {
+          SWE.push(student.split(',')[0]);
+        }
+      }
+      console.log(`Number of students in CS: ${CS.length}. List: ${CS.join(', ')}`);
+      console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`);
+    })
+  
+  })
 }
 
 module.exports = countStudents;
